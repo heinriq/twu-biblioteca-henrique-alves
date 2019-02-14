abc878b6eac9027d2c244a11b5500f715e3afc17
Henrique Alves <lahenriquealves@Henriques-MacBook-Pro.local>
2019-02-15 05:07:28 +0530

    1.2 list of books

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 import java.io.*;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,10 +12,20 @@ import static org.hamcrest.CoreMatchers.*;
 public class ExampleTest {
 
     private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private ArrayList<Book> books = new ArrayList<Book>();
 
     @Before
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
+    }
+
+    @Before
+    public void BooksMustBeAddedWithGameOfThronesTitles() {
+        for (int volume=1; volume<=7; volume++) {
+            Book book = new Book();
+            book.setTitle("Game of Thrones vol: " + volume);
+            books.add(book);
+        }
     }
 
     @Test
@@ -28,5 +39,26 @@ public class ExampleTest {
         biblioteca.main(null);
         String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great books titles in Bangalore!\n";
         assertThat(outContent.toString(), is(equalTo(welcomeMessage)));
+    }
+
+    @Test
+    public void BookMustBeGameOfThrones() {
+        Book book = new Book();
+        book.setTitle("Game of Thrones");
+        String title = "Game of Thrones";
+
+        assertThat(title, is(equalTo(book.getTitle())));
+    }
+
+    @Test
+    public void sizeOfBooksListMustBeSeven() {
+        assertThat(7, is(equalTo(books.size())));
+    }
+
+    @Test
+    public void allBooksMustBeGameOfThronesAndYourVolume() {
+        for(int volume=0; volume<7; volume++) {
+            assertThat("Game of Thrones vol: " + (volume + 1), is(equalTo(books.get(volume).getTitle())));
+        }
     }
 }
